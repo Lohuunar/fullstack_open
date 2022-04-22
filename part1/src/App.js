4fda910ff66e94a0539ddaa29dcfa57b71c5aa70
Lohuunar <francodmsoloing@gmail.com>
2022-04-21 21:44:39 -0300

    yas

---
+++ part1/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Hello(props){
+  return(
+    <div>      
+      <p>
+        Hello {props.name}
+      </p>    
+    </div>
+  )
+}
+function App() {
+  const name = 'Ileana'  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Greetings</h1>     
+        <Hello name={name}  />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
